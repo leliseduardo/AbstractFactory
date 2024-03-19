eb6d244a32c54f077ead85539744557797be7ffc
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-03-18 23:00:16 -0300

    abstract factory create

---
+++ src/test/java/org/example/abstractfactory/AbstractFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.abstractfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
